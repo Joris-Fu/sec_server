e7405386df354755337f7ee7fbea40e99398f38a
zhangqunshi <zhangqunshi@126.com>
2017-05-24 18:14:34 +0800

    更改字段长度从256到255，因为mysql的主键最大长度为255。另外instance表增加了一些字段。

--- security_system/knowledgeBase/models.py
+++ security_system/knowledgeBase/models.py
@@ -4,52 +4,57 @@ from django.db import models
 
 
 class vendor(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    country = models.CharField(max_length=256, null=True)
-    url = models.CharField(max_length=256, null=True)
+    name = models.CharField(max_length=255, primary_key=True)
+    country = models.CharField(max_length=255, null=True)
+    url = models.CharField(max_length=255, null=True)
 
 
 class device(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    vendor = models.CharField(max_length=256, null=True)
-    deviceType = models.CharField(max_length=256, null=True)
+    name = models.CharField(max_length=255, primary_key=True)
+    vendor = models.CharField(max_length=255, null=True)
+    deviceType = models.CharField(max_length=255, null=True)
 
 
 class instance(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    vendor = models.CharField(max_length=256, null=True)
-    ins_type = models.CharField(max_length=256, null=True)
-    ip = models.CharField(max_length=256, null=True)
-    city = models.CharField(max_length=256, null=True)
-    country = models.CharField(max_length=256, null=True)
-    continent = models.CharField(max_length=256, null=True)
-    asn = models.CharField(max_length=256, null=True)
-    lat = models.CharField(max_length=256, null=True)
-    lon = models.CharField(max_length=256, null=True)
-    hostname = models.CharField(max_length=256, null=True)
-    service = models.CharField(max_length=256, null=True)
-    os = models.CharField(max_length=256, null=True)
-    app = models.CharField(max_length=256, null=True)
-    extrainfo = models.CharField(max_length=256, null=True)
-    version = models.CharField(max_length=256, null=True)
-    port = models.CharField(max_length=256, null=True)
-    banner = models.CharField(max_length=1024, null=True)
-    timestamp = models.CharField(max_length=256, null=True)
-    type_index = models.CharField(max_length=256, null=True)
+    name = models.CharField(max_length=255, primary_key=True)
+    vendor = models.CharField(max_length=255, null=True, verbose_name='厂商')
+    ins_type = models.CharField(max_length=255, null=True, verbose_name='协议')
+    ip = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+    continent = models.CharField(max_length=255, null=True, verbose_name='洲')
+    asn = models.CharField(max_length=255, null=True)
+    lat = models.CharField(max_length=255, null=True, verbose_name='纬度')
+    lon = models.CharField(max_length=255, null=True, verbose_name='经度')
+    hostname = models.CharField(max_length=255, null=True)
+    service = models.CharField(max_length=255, null=True)
+    os = models.CharField(max_length=255, null=True, verbose_name='操作系统类型')
+    app = models.CharField(max_length=255, null=True)
+    extrainfo = models.CharField(max_length=255, null=True)
+    version = models.CharField(max_length=255, null=True)
+    port = models.CharField(max_length=255, null=True)
+    banner = models.TextField(null=True)
+    timestamp = models.CharField(max_length=255, null=True)
+    type_index = models.CharField(max_length=255, null=True)
+    organization = models.CharField(max_length=30, blank=True, null=True)
+    isp = models.CharField(max_length=30, blank=True, null=True)
+    add_time = models.DateTimeField(verbose_name='首次添加时间')
+    from_scan = models.IntegerField(blank=True, null=True, verbose_name='来自扫描')  # 0 -> False, 1 -> True
+    from_spider = models.IntegerField(blank=True, null=True, verbose_name='来自爬虫')  # 0 -> False, 1 -> True
 
 
 class vulnerability(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    vendor = models.CharField(max_length=256, null=True)
-    level = models.CharField(max_length=256, null=True)
+    name = models.CharField(max_length=255, primary_key=True)
+    vendor = models.CharField(max_length=255, null=True)
+    level = models.CharField(max_length=255, null=True)
     description = models.CharField(max_length=1024, null=True)
-    url = models.CharField(max_length=256, null=True)
-    mitigation = models.CharField(max_length=256, null=True)
-    provider = models.CharField(max_length=256, null=True)
-    date = models.CharField(max_length=256, null=True)
+    url = models.CharField(max_length=255, null=True)
+    mitigation = models.CharField(max_length=255, null=True)
+    provider = models.CharField(max_length=255, null=True)
+    date = models.CharField(max_length=255, null=True)
 
 
 class dev2vul(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    device = models.CharField(max_length=256)
-    vulnerability = models.CharField(max_length=256)
+    name = models.CharField(max_length=255, primary_key=True)
+    device = models.CharField(max_length=255)
+    vulnerability = models.CharField(max_length=255)
